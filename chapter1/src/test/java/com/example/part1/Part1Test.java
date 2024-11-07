edaeab94c1e43eb5b3bd5cc21ac3613493accf2f
Ludovic-ceccotti-Inetum <ludovic.ceccotti@inetum-realdolmen.world>
2024-11-07 15:37:33 +0100

    complete chapter 1

--- chapter1/src/test/java/com/example/part1/Part1Test.java
+++ chapter1/src/test/java/com/example/part1/Part1Test.java
@@ -1,9 +1,13 @@
 package com.example.part1;
 
-import org.apache.logging.log4j.core.LoggerContext;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,7 +17,30 @@ public class Part1Test {
 
     @Test
     public void test1() {
-        LOGGER.info(() -> "Running test" );
-        assertTrue(2 > 1);
+        Integer a,b;
+        a = 2;
+        b = 2;
+        assertEquals(0,Integer.compare(b,a));
+    }
+
+    @Test
+    public void testAliasing() {
+        List<String> l1 = new ArrayList<>();
+        l1.add("hello");
+        List<String> l2 = new ArrayList<>(l1);
+        LOGGER.info(l2::toString);
+        l1.add("bonjour");
+        l2 = l1; // l2 share the same reference to l1 so adding element to l1 will add this element to l2 too
+        l1.add("ciao");
+        LOGGER.info(l1::toString);
+        assertTrue(l2.contains("ciao"));
+    }
+
+    @Test
+    public void testQuestion1() {
+        int i = 0;
+        int [] ia = new int[] {0,1,2,3};
+        ia[++i] = ia[++i] + ia[i];
+        assertTrue(Arrays.toString(ia).equals("[0, 4, 2, 3]"));
     }
 }
